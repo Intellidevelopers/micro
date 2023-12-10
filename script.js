052965b979b23ccd1e22772216200115e883c8ae
Intellidevelopers <134317370+Intellidevelopers@users.noreply.github.com>
2023-12-10 20:23:16 +0100

    hh

--- script.js
+++ script.js
@@ -1,39 +1,40 @@
-import * as msal from 'https://cdn.skypack.dev/msal';
+// script.js
 
 const msalConfig = {
-  auth: {
-    clientId: 'c75726fa-2f09-4a02-b9c5-7683e091bc6e', // Replace with your client ID
-    authority: 'https://login.microsoftonline.com/common',
-    redirectUri: 'https://cloud-tech.vercel.app/', // Replace with your redirect URI
-  },
-  cache: {
-    cacheLocation: 'localStorage',
-    storeAuthStateInCookie: true,
-  },
-};
-
-const myMSALObj = new msal.PublicClientApplication(msalConfig);
-
-const loginRequest = {
-  scopes: ['user.read'],
-};
-
-function handleResponse(response) {
-  // Handle the authentication response
-  if (response !== null) {
-    console.log(response);
-    // You can perform additional tasks here after successful login
-  } else {
-    console.error('Authentication failed');
+    auth: {
+      clientId: 'YOUR_CLIENT_ID', // Replace with your client ID
+      authority: 'https://login.microsoftonline.com/common',
+      redirectUri: 'YOUR_REDIRECT_URI', // Replace with your redirect URI
+    },
+    cache: {
+      cacheLocation: 'localStorage',
+      storeAuthStateInCookie: true,
+    },
+  };
+  
+  const myMSALObj = new msal.PublicClientApplication(msalConfig);
+  
+  const loginRequest = {
+    scopes: ['user.read'],
+  };
+  
+  function handleResponse(response) {
+    // Handle the authentication response
+    if (response !== null) {
+      console.log(response);
+      // You can perform additional tasks here after successful login
+    } else {
+      console.error('Authentication failed');
+    }
   }
-}
-
-function signIn() {
-  myMSALObj.loginPopup(loginRequest)
-    .then(handleResponse)
-    .catch(error => {
-      console.error(error);
-    });
-}
-
-document.getElementById('signInButton').addEventListener('click', signIn);
+  
+  function signIn() {
+    myMSALObj.loginPopup(loginRequest)
+      .then(handleResponse)
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  
+  document.getElementById('signInButton').addEventListener('click', signIn);
+  
